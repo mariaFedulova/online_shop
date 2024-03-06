c5ce16f7d0332428bf6b93f60ac433ad5ea85f05
mariaFedulova <mary@fedulovs.com>
2024-03-06 19:43:33 +0300

    catalog

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
+//navigation
 let menu_button = document.getElementById('menu-button');
 let menu_close_button = document.getElementById('close-button');
 let navigation = document.getElementById('menu');
 
+//catalog filter menu
+let filter_open_button = document.getElementById('filter-open');
+let filter_close_button = document.getElementById('filter-close');
+let filter_menu = document.getElementById('filter-menu');
+
+
+//catalog filter option
+let option_btns = document.getElementsByClassName("catalog__filter-option");
+let option_lists = document.getElementsByClassName("filter-list");
+
+//navigation
 menu_button.addEventListener('click', function () {
     navigation.classList.toggle('display-block');
-});
+})
 
 menu_close_button.addEventListener('click', function () {
     navigation.classList.toggle('display-block');
 })
 
-// function changeItem() {
-//     document.getElementById('menu').style.display = 'block';
-// }
-// function rechangeItem() {
-//     document.getElementById('menu').style.display = 'none';
-// }
+//catalog filter
+filter_open_button.addEventListener('click', function () {
+    filter_menu.classList.add('display-flex');
+})
+
+filter_close_button.addEventListener('click', function () {
+    filter_menu.classList.remove('display-flex');
+})
+
+//filter options
+for (let i = 0; i < option_btns.length; i++) {
+    let btn = option_btns[i];
+    btn.onclick = function () {
+        option_lists[i].classList.toggle('display-flex');
+    }
+}
